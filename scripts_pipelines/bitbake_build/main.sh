447b0a45d9084288d2fbf473a330573a0899d8fe
Goran Paukovic <goran.paukovic@ctek.com>
2022-10-31 10:25:16 +0100

    Restructure pipeline
    Restructured pipeline

--- scripts_pipelines/bitbake_build/main.sh
+++ scripts_pipelines/bitbake_build/main.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env sh
 docker run -i \
            -v /opt/yocto_shares/sstate-cache:/opt/yocto_shares/sstate-cache \
            -v /opt/yocto_shares/downloads:/opt/yocto_shares/downloads \
            -v $PWD:/workdir \
            --workdir=/workdir \
            oe-build-goran:1.0 \
-           ./build_oe-core.sh
+           ./scripts_pipelines/bitbake_build/build_oe-core.sh
--- scripts_pipelines/unit_tests/main.sh
+++ scripts_pipelines/unit_tests/main.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env sh
 docker run -i \
            --user $(id -u):$(id -g) \
            -v $PWD/unit_tests:/workdir/unit_tests \
            -v $PWD/scripts_pipelines:/workdir/scripts_pipelines \
            --workdir=/workdir python:3.12.0a1-alpine3.16 \
-           ./scripts_pipelines/execute_tests.sh
+           ./scripts_pipelines/unit_tests/execute_tests.sh
