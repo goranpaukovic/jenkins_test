9d2dba0b8cb7692f7d0691ffa9235bf26189379c
Goran Paukovic <goran.paukovic@ctek.com>
2022-11-01 15:26:22 +0100

    Docker fix 3

--- scripts_pipelines/bitbake_build/main.sh
+++ scripts_pipelines/bitbake_build/main.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 job_name=$1
+docker_image_name="bitbake_build-${job_name,,}"
+echo "Create a container from image $docker_image_name"
 docker run -i \
            -v /opt/yocto_shares/sstate-cache:/opt/yocto_shares/sstate-cache \
            -v /opt/yocto_shares/downloads:/opt/yocto_shares/downloads \
            -v $PWD:/workdir \
            --workdir=/workdir \
-           bitbake_build-${job_name,,} \
+           $docker_image_name \
            ./scripts_pipelines/bitbake_build/build_oe-core.sh
--- scripts_pipelines/unit_tests/main.sh
+++ scripts_pipelines/unit_tests/main.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 job_name=$1
+docker_image_name="unit_tests-${job_name,,}"
+echo "Create a container from image $docker_image_name"
 docker run -i \
            --user $(id -u):$(id -g) \
            -v $PWD/unit_tests:/workdir/unit_tests \
            -v $PWD/scripts_pipelines:/workdir/scripts_pipelines \
            --workdir=/workdir \
-           unit_tests-${job_name,,} \
+           $docker_image_name \
            ./scripts_pipelines/unit_tests/execute_tests.sh
